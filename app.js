5cf71992550cdaeae67ee17ceb7d74f7543ca5b9
Fernando <f.sanchezvilela@gmail.com>
2019-11-12 23:48:21 +0100

    add and get from local sotrage

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 // Storage Controller
 const StorageCtrl = (function () {
+  //Public methods
+  return {
+    storeItem: function (item) {
+      let items;
+      // Check if any items in ls
+      if (localStorage.getItem('items') === null) {
+        items = [];
+        // Push new item
+        items.push(item);
+        // Set ls
+        localStorage.setItem('items', JSON.stringify(items));
+      } else {
+        // Get what is already in ls
+        items = JSON.parse(localStorage.getItem('items'));
 
+        // Push the new item
+        items.push(item);
+
+        // Re set ls
+        localStorage.setItem('items', JSON.stringify(items));
+      }
+      localStorage.getItem('items');
+    },
+    getItemsFromStorage: function () {
+      let items;
+      if (localStorage.getItem('items') === null) {
+        items = [];
+      } else {
+        items = JSON.parse(localStorage.getItem('items'))
+      }
+      return items;
+    }
+  }
 })()
 
-
 // Item Controller
-// Private
 const ItemCtrl = (function () {
   // Item Constructor
   const Item = function (id, name, calories) {
@@ -16,11 +46,12 @@ const ItemCtrl = (function () {
 
   // Data Structure / State
   const data = {
-    items: [
-      // {id:0, name:'Steak Dinner', calories:1200},
-      // {id:1, name:'Cookie', calories:400},
-      // {id:2, name:'Eggs', calories:300},
-    ],
+    // items: [
+    //   // {id:0, name:'Steak Dinner', calories:1200},
+    //   // {id:1, name:'Cookie', calories:400},
+    //   // {id:2, name:'Eggs', calories:300},
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   }
@@ -257,9 +288,8 @@ const UICtrl = (function () {
   }
 })()
 
-
 // App Controller
-const App = (function (ItemCtrl, UICtrl) {
+const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
   //Private
   //Load Event Listeners
   const loadEventListeners = function () {
@@ -305,10 +335,15 @@ const App = (function (ItemCtrl, UICtrl) {
       const newItem = ItemCtrl.addItem(input.name, input.calories);
       // Add item to UI list
       UICtrl.addListItem(newItem);
+
       // Get total calories
       const totalCalories = ItemCtrl.getTotalCalories();
       // Add total calories to UI
       UICtrl.showTotalCalories(totalCalories);
+
+      //Store in localStorage
+      StorageCtrl.storeItem(newItem);
+
       // Clear fields
       UICtrl.clearInput();
     }
@@ -397,6 +432,11 @@ const App = (function (ItemCtrl, UICtrl) {
     init: function () {
       // Clear edited state / set initial set
       UICtrl.clearEditState();
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
       console.log('Initializing App...');
       // Fetch items from data structure;
       const items = ItemCtrl.getItems();
@@ -414,7 +454,7 @@ const App = (function (ItemCtrl, UICtrl) {
     }
   }
 
-})(ItemCtrl, UICtrl);
+})(ItemCtrl, StorageCtrl, UICtrl);
 
 // Initialize App
 App.init();
